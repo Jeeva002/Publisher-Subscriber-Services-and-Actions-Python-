1f31d03cac82f956083f74a9aa67a17dcbe7f331
Jeeva <122138371+Jeeva002@users.noreply.github.com>
2024-11-26 12:14:24 +0530

    Create service_client.py

---
+++ service_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from std_srvs.srv import Empty
+
+def service_client():
+    rospy.wait_for_service('my_service')
+    try:
+        service = rospy.ServiceProxy('my_service', Empty)
+        service()
+        rospy.loginfo("Service call successful.")
+    except rospy.ServiceException as e:
+        rospy.logerr("Service call failed: %s", e)
+
+if __name__ == '__main__':
+    service_client()
